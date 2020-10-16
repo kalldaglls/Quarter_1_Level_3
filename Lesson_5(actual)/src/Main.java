bf6359e1a7bef20663e2865578887f0fb5eaae51
kalldaglls <kalldaglls@mail.ru>
2020-10-16 16:31:30 +0300

    Les_5

--- Lesson_5(actual)/src/Main.java
+++ Lesson_5(actual)/src/Main.java
@@ -1,13 +1,31 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
 public class Main {
     public static void main(String[] args) {
-
+        Semaphore semaphore = new Semaphore(3);
+        FuelStation fuelStation = new FuelStation(semaphore);
         ExecutorService executorService = Executors.newCachedThreadPool();
-        for (int i = 0; i < ; i++) {
-            
+        for (int i = 0; i < 3 ; i++) {
+            executorService.submit(
+                    new Bus("Bus #" + i, fuelStation)
+            );
         }
+
+        for (int i = 0; i < 3 ; i++) {
+            executorService.submit(
+                    new Car("Car #" + i, fuelStation)
+            );
+        }
+
+        for (int i = 0; i < 3 ; i++) {
+            executorService.submit(
+                    new Truck("Truck #" + i, fuelStation)
+            );
+        }
+
+        executorService.shutdown();
 
     }
 }
